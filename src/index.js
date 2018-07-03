a60e87641edd44df3469e4d1c402473109c2c26e
Roger <luojie.5408@163.com>
2018-07-03 15:50:16 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import dva from 'dvax'
+import dva,{ Model, Fetch } from 'dvax'
 import App from './App'
+import model from './model'
 import './global.css'
+
+const fetch = Fetch({ 
+    baseUrl: `http://1.1.1.1`,
+    bodyParser(data){
+        const postdata = new FormData()
+        for (let k in data) {
+            if (data.hasOwnProperty(k)) {
+                postdata.append(k, data[k])
+            }
+        }
+        return postdata
+    }
+})
+dva.start({ sagaInjection: { fetch } })
+Model.create(model)
+
 render(
     <Provider store={dva._store}>
         <App />
---
+++ src/model.js
@@ -0,0 +1,20 @@
+// import { initListData } from 'components/list'
+// import { replace } from 'components/editor'
+
+export default {
+    namespace: 'app',
+    state: {
+        showSearchPanel: false,
+        content: '',
+        // listComponent: ()=>null,
+        // editorComponent: ()=>null
+    },
+    reducers: {
+    },
+    effects: {},
+    event: {
+        onReady(dispatch) {
+            // 应该在高层调用低层应用方法
+        }
+    }
+}
