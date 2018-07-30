b88e6828b95032763c7102a0ea998fecd14ee25a
Roger <luojie.5408@163.com>
2018-07-30 16:56:34 +0800

    update

--- babelRegister.js
+++ babelRegister.js
@@ -1,3 +1,3 @@
-require("babel-register")({ // .babelrc是用来给babel-register用的, 默认不转node_modules，dvax就不兼容了
-    ignore: /node_modules\/(?!.*dvax)/ //dvax前面可能有其他字符,.*表示任意个数的任意字符
+require("babel-register")({ // 配合.babelrc使用, 默认不转node_modules
+    ignore: /node_modules\/(?!.*dvax)/ // dvax前面可能有其他字符,cnpm时产生版本数组, .*表示任意个数的任意字符
 })
