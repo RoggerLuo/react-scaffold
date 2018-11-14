9e29c46a5af10160fc5cb2337b048f86e1e3f687
Roger <luojie.5408@163.com>
2018-11-14 10:26:31 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import './global.css'
-import './style.css'
 import { Model } from 'dvax'
+import styled from 'styled-components'
 import model from './model'
 Model.create(model)
+
+const Margin = styled.div`
+    margin:20px;
+`
+const Link = styled.a`
+    text-decoration: none;
+    color: black;
+`
+
 class App extends React.Component { 
     constructor(props) {
         super(props)
     }
     render() {
-        return <h2>{'Welcome'}</h2>
+        return (<Margin>
+            <h2>{'Welcome'}</h2>
+            <Link href="https://www.npmjs.com/package/dvax" target="_blank">查看文档</Link>
+        </Margin>)
     }
 }
 export default App
