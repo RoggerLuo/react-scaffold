0c511ff07910e2e238e0e1da293ff959eb03b069
Roger <luojie.5408@163.com>
2018-11-28 22:13:42 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import "regenerator-runtime/runtime"
 import 'es6-promise/auto'
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
 import dvax from 'dvax'
-import fetch from 'utils/fetch'
+import Fetch from 'dvax/fetch'
 import App from './App'
 
+const fetch = Fetch({ 
+    baseUrl: 'localhost:5555', 
+    headers: {
+        "Content-Type": "application/json",
+    },
+    requestBody(body){
+        return JSON.stringify(body)
+    },
+    receiveData(res){
+        if(res.hasErrors) {
+            alert('接口错误')
+        }
+        return res
+    }
+})
 const config = { effects: { fetch } }
 const DvaxApp = dvax.start(<App/>,config)
 render(DvaxApp,document.getElementById('root'))
